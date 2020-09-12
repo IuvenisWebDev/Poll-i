1ea979e342bc09b4f419d6443f77de5e7b21b993
Kóródi Lőrinc <korodilorinc@gmail.com>
2020-09-12 18:10:30 +0200

    Options added to mongoose.connect

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pollRouter = require('./routers/pollRouter')
 
 const app = express();
 
-mongoose.connect(`mongodb://localhost/voteApp`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb://localhost/voteApp`, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false,useCreateIndex: true})
     .then((result) =>{
 
         console.log('Connected to database');

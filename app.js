ac62590e8f9602cd7615a68cef4bbe37efdf3fdc
Mark Szavin <szavinm@yahoo.com>
2020-09-24 21:41:37 +0200

    minor corrections

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const userRouter = require('./routers/userRouter');
-const pollRouter = require('./routers/pollRouter')
+const userRouter = require("./routers/userRouter");
+const pollRouter = require("./routers/pollRouter");
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.heuxh.mongodb.net/VoteApp?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false,useCreateIndex: true})
-    .then((result) =>{
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.heuxh.mongodb.net/VoteApp?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then((result) => {
+    console.log("Connected to database");
 
-        console.log('Connected to database');
+    app.listen(process.env.PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-        app.listen(process.env.PORT)
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-    }).catch((err) =>{
+app.use("/user", userRouter);
 
-        console.log(err);
+app.use("/poll", pollRouter);
 
-    })
-
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'));
-
-app.use('/user', userRouter);
-
-app.use('/poll', pollRouter);
-
-app.get('/', (req, res) =>{
-
-    res.sendFile('/html/landingpage.html',{root: "./"});
-
+app.get("/", (req, res) => {
+  res.sendFile("/html/landingpage.html", { root: "./" });
 });
 
-app.use((req,res) => {
-
-    res.status(404).end(); 
-
+app.use((req, res) => {
+  res.status(404).end();
 });

d00ec4a210e75dcc35b774d294faed8844552083
Mark Szavin <szavinm@yahoo.com>
2020-10-19 11:12:05 +0200

    send static html after successful login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,62 +1,44 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const path = require("path");
 
 const signUp = async (req, res) => {
-    try{
+  try {
+    const user = new User(req.body);
 
-        const user = new User(req.body);
+    if (await User.findOne({ email: user.email })) {
+      res.status(409).end();
+    } else {
+      user.password = await bcrypt.hash(user.password, 12);
 
-        if(await User.findOne({email: user.email})){
-
-            res.status(409).end();
-
-        }else{
-            user.password = await bcrypt.hash(user.password,12);
-
-            user.save().then(async (result) => {
-
-              
-                await res.cookie("user_id",result._id,{httpOnly: true,});
-                res.redirect("/");
-                
-
-            })
-        }
-
-    }catch (err){
-
-        console.log(err);
-
+      user.save().then(async (result) => {
+        await res.cookie("user_id", result._id, { httpOnly: true });
+        res.redirect("/");
+      });
     }
-      
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const login = async (req, res) => {
-    try{
-        const user = await User.findOne({email : req.body.email})
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
-        if(!user){
-
-            res.status(409).end();
-
-        }else if(! await bcrypt.compare(req.body.password, user.password)){
-
-            res.status(401).end();
-
-        }else{
-            
-            res.cookie("user_id",user._id);
-            res.send({user});
-
-        }
-    }catch (err){
-
-        console.log(err);
+    if (!user) {
+      res.status(409).end();
+    } else if (!(await bcrypt.compare(req.body.password, user.password))) {
+      res.status(401).end();
+    } else {
+      res.cookie("user_id", user._id);
+      res.sendFile(path.join(__dirname, "..", "public", "html", "home.html"));
     }
-    
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
-    signUp,
-    login
+  signUp,
+  login,
 };

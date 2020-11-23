d4e534fc6f60f059563323f9bea7b6282f9598d1
Mark Szavin <szavinm@yahoo.com>
2020-11-23 16:22:08 +0100

    add async await

--- public/client_side_script/menuHandler.js
+++ public/client_side_script/menuHandler.js
@@ -1,20 +1,23 @@
-document.querySelector("#mypolls-menu").addEventListener("click", () => {
+document.querySelector("#mypolls-menu").addEventListener("click", async () => {
   mainContent.innerHTML = "";
 
-  axios
+  await axios
     .get("/poll/current/polls")
-    .then((res) => (mainContent.innerHTML = renderPolls(res.data)))
-    .catch((err) => console.log(err));
+    .then(res => (mainContent.innerHTML = renderPolls(res.data)))
+    .catch(err => console.log(err));
 });
 
-document.querySelector("#openpolls-menu").addEventListener("click", () => {
-  mainContent.innerHTML = "";
+document
+  .querySelector("#openpolls-menu")
+  .addEventListener("click", async () => {
+    mainContent.innerHTML = "";
 
-  axios
-    .get("/poll")
-    .then(
-      (res) =>
-        (mainContent.innerHTML = renderPolls(res.data, { voteOptions: true }))
-    )
-    .catch((err) => console.log(err));
-});
+    await axios
+      .get("/poll")
+      .then(
+        res =>
+          (mainContent.innerHTML = renderPolls(res.data, { voteOptions: true }))
+          
+      )
+      .catch(err => console.log(err));
+  });

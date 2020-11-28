145b3a38883ebe0985bb5fde83e478f37e4b36f5
Mark Szavin <szavinm@yahoo.com>
2020-11-28 16:30:23 +0100

    create class  Message

---
+++ public/client_side_script/message.js
@@ -0,0 +1,24 @@
+class Message {
+  constructor(text, type, target, displayTime) {
+    this.text = text;
+    this.type = type;
+    this.target = target;
+    this.displayTime = displayTime;
+  }
+
+  displayMessage = () => {
+    let msgContainer = document.createElement("div");
+    msgContainer.innerHTML = `
+    <div id="msg-alert" class="alert ${this.type} text-center col-10 ml-3 mt-4" role="alert">
+    ${this.text}
+    </div>
+    `;
+
+    this.target.appendChild(msgContainer);
+
+    setTimeout(() => {
+      let messageToRemove = document.querySelector("#msg-alert");
+      messageToRemove.remove();
+    }, this.displayTime);
+  };
+}

2419227a6d39f89b3cfeac79a413ec473dccc501
korodilorinc <korodilorinc@gmail.com>
2020-12-02 20:53:29 +0100

    Send error message in res

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,12 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
-const path = require("path");
 
 const signUp = async (req, res) => {
   try {
     const user = new User(req.body);
 
     if (await User.findOne({ email: user.email })) {
-      res.status(409).end();
+      res.status(409).send({error: "This e-mail is already in use."});
     } else {
       user.password = await bcrypt.hash(user.password, 12);
 
@@ -21,18 +20,18 @@ const signUp = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {
+const login = async (req, res,next) => {
   try {
 
     const user = await User.findOne({ email: req.body.email });
 
     if (!user) {
 
-      res.status(409).end();
+      res.status(409).send({error: "User not found."});
 
     } else if (!(await bcrypt.compare(req.body.password, user.password))) {
 
-      res.status(401).end();
+      res.status(401).send({error: "Invalid password."});
 
     } else {
 
@@ -41,18 +40,18 @@ const login = async (req, res) => {
       res.redirect("/");
     }
   } catch (err) {
-    console.log(err);
+    next(err);
   }
 };
 
-const signOut = async (req,res) =>{
+const signOut = async (req,res,next) =>{
   try{
 
     res.clearCookie("user_id");
     res.clearCookie("user_name");
     res.redirect("/");
   }catch (err){
-    console.log(err);
+    next(err);
   }
 }
 
